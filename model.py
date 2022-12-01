cf60ebd8a63ec04396d2c22f4fe4b9bd4a50a3bc
stpdety <hongyi.shi@epfl.ch>
2022-12-01 14:56:17 +0100

    model done

--- model.py
+++ model.py
@@ -1,73 +1,87 @@
 import torch.nn as nn
-import torch.nn.functional as F
+import torch
 
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv3d(3, 32, 3)
-        nn.BatchNorm3d(32)
-        nn.ReLU()
-        nn.Conv3d(32, 64, 3)
-        nn.BatchNorm3d(64)
-        nn.ReLU()
         
-        nn.MaxPool3(2, stride=2)
+        self.down = MaxPool3(2, stride=2)
+        self.up = nn.ConvTranspose3d(2, stride=2)
         
-        nn.Conv3d(64, 64, 3)
-        nn.BatchNorm3d(64)
-        nn.ReLU()
-        nn.Conv3d(64, 128, 3)
-        nn.BatchNorm3d(128)
-        nn.ReLU()
+        #layer1
+        self.layer11 = self.conv_relu(3, 32)
+        self.layer12 = self.conv_relu(32, 64)
+                
+        #layer2
+        self.layer21 = self.conv_relu(64, 64)
+        self.layer22 = self.conv_relu(64, 128)
         
-        nn.MaxPool3(2, stride=2)
+        #layer3
+        self.layer31 = self.conv_relu(128, 128)
+        self.layer32 = self.conv_relu(128, 256)
         
-        nn.Conv3d(128, 128, 3)
-        nn.BatchNorm3d(128)
-        nn.ReLU()
-        nn.Conv3d(128, 256, 3)
-        nn.BatchNorm3d(256)
-        nn.ReLU()
+        #layer4
+        self.layer41 = self.conv_relu(256, 256)
+        self.layer42 = self.conv_relu(256, 512)
         
-        nn.MaxPool3(2, stride=2)
+        #layer5
+        self.layer51 = self.conv_norm_relu(256+512, 256)
+        self.layer52 = self.conv_norm_relu(256, 256)
         
-        nn.Conv3d(256, 256, 3)
-        nn.BatchNorm3d(256)
-        nn.ReLU()
-        nn.Conv3d(256, 512, 3)
-        nn.BatchNorm3d(512)
-        nn.ReLU()
+        #layer6
+        self.layer61 = self.conv_norm_relu(128+256, 128)
+        self.layer62 = self.conv_norm_relu(128, 128)
         
-        nn.ConvTranspose3d(2, stride=2)
+        #layer7
+        self.layer11 = self.conv_norm_relu(64+128, 64)
+        self.layer12 = self.conv_norm_relu(64, 64)
         
-        nn.Conv3d(256+512, 256, 3)
-        nn.BatchNorm3d(256)
-        nn.ReLU()
-        nn.Conv3d(256, 256, 3)
-        nn.BatchNorm3d(256)
-        nn.ReLU()
+        self.conv = nn.Conv3d(64, 3, 1)
         
-        nn.ConvTranspose3d(2, stride=2)
-        
-        nn.Conv3d(128+256, 128, 3)
-        nn.BatchNorm3d(128)
-        nn.ReLU()
-        nn.Conv3d(128, 128, 3)
-        nn.BatchNorm3d(128)
-        nn.ReLU()
-        
-        nn.ConvTranspose3d(2, stride=2)
-        
-        nn.Conv3d(64+128, 64, 3)
-        nn.BatchNorm3d(64)
-        nn.ReLU()
-        nn.Conv3d(64, 64, 3)
-        nn.BatchNorm3d(64)
-        nn.ReLU()
-        
-        nn.Conv3d(64, 3, 1)
-        
+    def conv_relu(self, feat_in, feat_out):
+        return nn.Sequential(
+            nn.nn.Conv3d(feat_in, feat_out, kernel_size=3),
+            nn.BatchNorm3d(feat_out),
+            nn.ReLU())
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        return F.relu(self.conv2(x))
+        out = self.layer11(x)
+        out = self.layer12(out)
+        sc1 = out
+        
+        out = self.down(out)
+        
+        out = self.layer21(x)
+        out = self.layer22(out)
+        sc2 = out
+        
+        out = self.down(out)
+        
+        out = self.layer31(x)
+        out = self.layer32(out)
+        sc3 = out
+        
+        out = self.down(out)
+        
+        out = self.layer41(x)
+        out = self.layer42(out)
+        
+        out = self.up(out)
+        
+        out = torch.cat((out, sc3), 1)
+        out = self.layer51(x)
+        out = self.layer52(out)
+        
+        out = self.up(out)
+        
+        out = torch.cat((out, sc2), 1)
+        out = self.layer61(x)
+        out = self.layer62(out)
+        
+        out = self.up(out)
+        
+        out = torch.cat((out, sc1), 1)
+        out = self.layer71(x)
+        out = self.layer72(out)
+        
+        return self.conv(out)
